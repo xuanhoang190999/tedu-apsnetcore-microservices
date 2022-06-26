deca265180df1a305be653546f8850f3cfc5dd1e
Xuan Hoang <hoangdx@tmtsofts.com>
2022-06-26 17:41:12 +0700

    Use Serilog

---
+++ apsnetcore-microservices/src/Services/Inventory/Inventory.Product.API/Program.cs
@@ -0,0 +1,46 @@
+
+using Common.Logging;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog(Serilogger.Configure);
+
+Log.Information("Start Inventory API up");
+
+try
+{
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down Inventory API complete");
+    Log.CloseAndFlush();
+}
+
+
